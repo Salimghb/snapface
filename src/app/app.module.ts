7b81b3a8ed252e797088a4f40e1d93017e1089f4
Salim Ghbabra <sghbabra@hotmail.fr>
2022-04-22 15:28:24 +0200

    Formatez les dates :
    https://openclassrooms.com/fr/courses/7471261-debutez-avec-angular/7549366-formatez-les-dates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import * as fr from '@angular/common/locales/fr'
 
 @NgModule({
   declarations: [AppComponent, FaceSnapComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr-FR',
+    },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+}
