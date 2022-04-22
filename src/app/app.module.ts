d3747050a21e8317e06dd6122594d442f7a31cf9
Salim Ghbabra <sghbabra@hotmail.fr>
2022-04-22 13:38:24 +0200

    Réagissez aux évènements : https://openclassrooms.com/fr/courses/7471261-debutez-avec-angular/7549301-reagissez-aux-evenements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceSnapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  buttonText!: string;
+  imageUrl!: string;
+
+  ngOnInit(): void {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.buttonText ='Like';
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+
+  onSnap() {
+
+    const snapped = 'Unlike';
+    const notSnapped = 'Like';
+
+    if (this.buttonText === snapped) {
+      this.snaps--;
+      this.buttonText = notSnapped;
+    } else {
+      this.snaps++;
+      this.buttonText = snapped;
+    }
+
+  }
+}
