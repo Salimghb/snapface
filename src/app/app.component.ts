44d9356ab19102b3085b69ebf6e85208be06abef
Salim Ghbabra <sghbabra@hotmail.fr>
2022-04-22 14:55:13 +0200

    Ajoutez du style dynamique :
    https://openclassrooms.com/fr/courses/7471261-debutez-avec-angular/7549326-ajoutez-du-style-dynamique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
       description: 'Mon meilleur ami depuis tout petit !',
       imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       createdDate: new Date(),
-      snaps: 0,
+      snaps: 3,
       location: 'Paris'
     },
     {
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit {
       description: 'Un endroit magnifique pour les randonnées.',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
       createdDate: new Date(),
-      snaps: 0,
+      snaps: 4,
       location: 'In the mountains 😉'
     },
     {
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
       description: 'Mmmh que c\'est bon !',
       imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
       createdDate: new Date(),
-      snaps: 0
+      snaps: 1
     }];
   }
 }
