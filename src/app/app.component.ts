90656d921211f18eb3ab51989b14afe824afda70
Salim Ghbabra <sghbabra@hotmail.fr>
2022-04-22 14:35:57 +0200

    Conditionnez l'affichage : https://openclassrooms.com/fr/courses/7471261-debutez-avec-angular/7549311-conditionnez-laffichage-des-elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  mySnap1!: FaceSnap;
-  mySnap2!: FaceSnap;
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap1 = new FaceSnap(
-      'Archibald1',
-      'Mon meilleur ami depuis tout petit !1',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
-    this.mySnap2 = new FaceSnap(
-      'Archibald2',
-      'Mon meilleur ami depuis tout petit !2',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
+    this.mySnap = {
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location: 'In the mountains 😉'
+    };
+    this.myOtherSnap = {
+      title: 'Three Rock Mountain',
+      description: 'Un endroit magnifique pour les randonnées.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 0,
+      location:'In the mountains 😉'
+    };
+    this.myLastSnap = {
+      title: 'Un bon repas',
+      description: 'Mmmh que c\'est bon !',
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 0
+    };
   }
 }
