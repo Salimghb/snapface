4772bbcbc667495438548259ee030e173a4a5f22
Salim Ghbabra <sghbabra@hotmail.fr>
2022-04-22 16:36:08 +0200

    Passez en SPA avec le routing :
    https://openclassrooms.com/fr/courses/7471261-debutez-avec-angular/7549406-passez-en-spa-avec-le-routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'facesnaps', component: FaceSnapListComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
-import * as fr from '@angular/common/locales/fr';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
-import { HeaderComponent } from './header/header.component'
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [AppComponent, FaceSnapComponent, FaceSnapListComponent, HeaderComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    FaceSnapComponent,
+    FaceSnapListComponent,
+    HeaderComponent,
+    LandingPageComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [
     {
       provide: LOCALE_ID,
